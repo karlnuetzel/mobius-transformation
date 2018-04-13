a3092a4af1da98ec74c95c4f5e1415b20ebb2b87
Alex Nuetzel <karlanuetzel@gmail.com>
2018-04-12 21:51:30 -0500

    grid system

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-/**
- * Created by Alex on 4/12/2018.
- */
+$(document).ready(function() {
+    var height = $('.quadOne').height();
+    var amount = height / 20;
+    console.log(amount);
+    console.log(height);
+    for (i = 0; i < 200; i++){
+        $('.quadOne').prepend("<div id='grid'></div>");
+        $('.quadTwo').prepend("<div id='grid'></div>");
+        $('.quadThree').prepend("<div id='grid'></div>");
+        $('.quadFour').prepend("<div id='grid'></div>");
+    }
+});
