4717a6016c623f753c75eb1b55f06a0c52ef99e2
bfoley4 <bdfhdc@mail.missouri.edu>
2018-04-13 01:06:42 -0500

    Translations

--- script.js
+++ script.js
@@ -45,7 +45,6 @@ $(document).ready(function() {
             });
         }
 
-
         function translateLeft() {
             $('.square').animate({
                 left: "-=500"
@@ -53,32 +52,53 @@ $(document).ready(function() {
             });
         }
 
-        function translateCurve(x, y) {
-            var direction = Math.atan2(x, y) / Math.PI * 180
-            var distance = Math.hypot(origin_x - x, origin_y - y);
-            console.log('Direction: ' + direction);
-            console.log('Distance: ' + distance);
+        // Variables involved in calculating curve
+        var x = 500;// destination x
+        var y = 500;// destination y
+        var offset = $('.square').offset();
+        var width = $('.square').width();
+        var height = $('.square').height();
+        var original_x = offset.left + width / 2;
+        var original_y = offset.top + height / 2;
+        var bezier_x = 150;
+        var bezier_y = 150;
 
-            function calculate() {
+        function translateCurve() {
+            // var direction = Math.atan2(x,  y) / Math.PI * 180;
+            // var distance = Math.hypot(original_x - x, original_y - y);
+            // console.log('Direction: ' + direction);
+            // console.log('Distance: ' + distance);
 
+            for(var t = 0.0; t <= 1; t += 0.01){
+                // Bezier curve calculations
+                var new_x = Math.pow(1-t, 2)*original_x + 2*(1-t)*t*bezier_x + Math.pow(t, 2)*x;
+                var new_y = Math.pow(1-t, 2)*original_y + 2*(1-t)*t*bezier_y + Math.pow(t,2)*y;
+
+                // $('.square').animate({
+                //     left: "+=" + new_x / 70
+                // }, 2, function () {});
+                //
+                // $('.square').animate({
+                //     top: "+=" + new_y / 100
+                // }, 2, function () {});
+
+                $('.square').animate({
+                    height: "+=4.5",
+                    width: "+=4.5",
+                    rotate:
+                }, 20, function () {});
             }
 
         }
 
         // Intro
-        translateLeft();
-        translateRight();
-        translateRight();
-        translateLeft();
+        // translateLeft();
+        // translateRight();
+        // translateRight();
+        // translateLeft();
 
-        var x = 500;
-        var y = 400;
-        var offset = $('.square').offset();
-        var height = $('.square').height();
-        var width = $('.square').width();
-        var origin_x = offset.left + width / 2;
-        var origin_y = offset.top + height / 2;
-        translateCurve(x, y);
+        translateCurve();
+
     }
 
 });
