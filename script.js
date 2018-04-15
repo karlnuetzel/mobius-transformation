987c50c9620bb678737083c0eefcefa6f17a683d
Alex Nuetzel <karlanuetzel@gmail.com>
2018-04-15 02:11:48 -0500

    code refactor and drawRectangle

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
 
     init();
     function init() {
+        var gridSize = 200;
         var x = 20;
         var y = 1;
         var xN = -1;
         var yN = -10;
 
-        for (i = 0; i < 200; i++) {
+        for (i = 0; i < gridSize; i++) {
             if (x === 0) {
                 x = 20;
                 xN = -1;
@@ -25,17 +26,19 @@ $(document).ready(function() {
             x--;
             xN--;
         }
-        drawCircle(8, 0, 0);
-        //var line = drawLine(1.5, 0);
-        //walkLine(line);
+        //call functions here
+        drawRectangle(10, 5, -4, -4)
     }
     function getLocation(x, y){
+
         var name = "_"+x+"_"+y+"";
         var coordinates = getCenter(name);
         return coordinates;
+
     }
 
     function getCenter(name){
+
         var height = $("."+name+"").height();
         var width = $("."+name+"").width();
         var offset = $("."+name+"").offset();
@@ -43,12 +46,15 @@ $(document).ready(function() {
         var y = offset.top + height / 2;
         var coordinates = {x: x, y: y};
         return coordinates;
+
     }
 
     function getGridElement(x, y){
+
         var name = "_"+x+"_"+y+"";
         var element = $("."+name+"");
         return element;
+
     }
 
     function getSquareLocation(){
@@ -60,131 +66,53 @@ $(document).ready(function() {
         var y = offset.top + height / 2;
         var coordinates = {x: x, y: y};
         return coordinates;
-    }
-
-    function walkPath(path){
 
     }
 
-    function drawRectangle(height, width , origin){
+    function drawRectangle(height, width , originX, originY){
 
-        var y = Math.round(origin + (height / 2))
-        var x = Math.round(origin + (width / 2));
-        for (i = 4; i < 4; i++){
-            drawLine()
-        }
+        var start = Math.round((originX - width / 2));
+        var end = Math.round((originX + width / 2));
+        drawLine(start, originY, end, originY);
+        drawLine(start, originY, start, height);
+        drawLine(end, originY, end, height);
+        drawLine(start, height, end, height);
 
     }
-
     function drawCircle(r, originX, originY){
 
-        var coordinates = getSquareLocation();
         for (i = 0; i < 360; i++){
 
             var x = Math.round(r * Math.sin(i)) + originX;
             var y = Math.round(r * Math.cos(i)) + originY;
-            // console.log(x);
-            // console.log(y);
             var element = getGridElement(x, y);
             element.css("background-color", "yellow");
+
         }
-    }
-    function drawLine(slope, y){
 
     }
-    function drawLinearLine(slope, y){
-        var listX = [];
-        var listY = [];
-        var listXN = [];
-        var listYN = [];
-        var yInt = 0;
-        var x = 1;
-        var xN = -1;
-        var num = 1;
-        var numN = 1;
-        while (num < 10.5 && numN < 10.5){
-            console.log(xN);
-            num = slope*x + y;
-            numN = slope*xN + y;
-            if (num < 10.5 && numN < 10.5) {
-                var location = Math.round(num + yInt);
-                var locationN = Math.round(numN + yInt);
-                var element = getGridElement(x, location);
-                var elementN = getGridElement(xN, locationN);
+    function drawLine(x1, y1, x2, y2){
+
+        var m = ((y2 - y1)/(x2  - x1));
+        b = (-1) * ((m * x2) - y2);
+        if (!isFinite(m)){
+            m = 0;
+            x = x1;
+            for (b = y1; b < y2; b++){
+
+                var y = Math.round((m*x) + b);
+                var element = getGridElement(x,y)
                 element.css("background-color", "yellow");
-                elementN.css("background-color", "yellow");
 
-
-                var square = getSquareLocation();
-                var newLoc = getCenter(element.attr("class"))
-                var newLocN = getCenter(element.attr("class"))
-
-
-
-
-                var moveX = Math.abs(square.x - newLoc.x);
-                var moveY = Math.abs(square.y - newLoc.y);
-                var moveXN = Math.abs(square.x - newLocN.x);
-                var moveYN = Math.abs(square.y - newLocN.y);
-
-                listX.push(moveX);
-                listXN.push(moveXN);
-                listY.push(moveY);
-                listYN.push(moveYN);
-                xN--;
-                x++;
             }
         }
-        var line = {x: listX, y: listY, xN: listXN, yN: listYN};
-        return line;
-    }
+        else {
+            for (x = x1; x < x2; x++){
 
-    function translateCurve() {
-        var loc = getLocation(17,9);
-        var square = getCenter("square");
-        console.log("original x " + square.x);
-        console.log("original y " + square.y);
-        console.log("destination x " + loc.x);
-        console.log("destination y " + loc.y);
-        var x = loc.x;// destination x
-        var y = loc.y;// destination y
-        var offset = $('.square').offset();
-        var width = $('.square').width();
-        var height = $('.square').height();
-        var original_x = offset.left + width / 2;
-        var original_y = offset.top + height / 2;
-        var bezier_x = 150;
-        var bezier_y = 150;
-
-        for(var t = 0.0; t <= 1; t += 0.01){
-            // Bezier curve calculations
-            var new_x = Math.pow(1-t, 2)*original_x + 2*(1-t)*t*bezier_x + Math.pow(t, 2)*x;
-            var new_y = Math.pow(1-t, 2)*original_y + 2*(1-t)*t*bezier_y + Math.pow(t,2)*y;
-
-            console.log("new_x" + new_x);
-            console.log("new_y" + new_x)
-
-
-            $('.square').animate({
-                left: "+=" + new_x / 70
-            }, 2, function () {});
-
-            $('.square').animate({
-                top: "+=" + new_y / 100
-            }, 2, function () {});
-
-            var offset1 = $('.square').offset();
-            var width1 = $('.square').width();
-            var height1 = $('.square').height();
-            var locx = offset1.left + width / 2;
-            var locy = offset1.top + height / 2;
-            console.log(locx);
-            console.log(locy);
-            // $('.square').animate({
-            //     height: "+=4.5",
-            //     width: "+=4.5"
-            // }, 20, function () {});
+                var y = Math.round((m*x) + b);
+                var element = getGridElement(x,y)
+                element.css("background-color", "yellow");
+            }
         }
-
     }
 });
